83403b491a75325a7abd1da7b4b7872335d9cf30
AngelLopez64 <angellopez148664@gmail.com>
2022-02-12 12:10:55 -0300

    asd

---
+++ Modeloss.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{ 
+			  breakpoint: 1980,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			  }
+			}
+		]
+	});
+});
